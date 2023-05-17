5203af50b39609f91d4b20039a7303f726aee805
mjaschew <dermijasch@gmx.de>
2023-05-17 15:51:24 +0200

    Changed QuizSession from service to model

--- backend/src/main/java/de/neuefische/backend/model/QuizParameter.java
+++ backend/src/main/java/de/neuefische/backend/model/QuizParameter.java
@@ -1,8 +1,12 @@
 package de.neuefische.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class QuizParameter {
     private String difficulty;
     private String gerne;
     private int numQuestion;
-
 }
--- backend/src/main/java/de/neuefische/backend/model/QuizSession.java
+++ backend/src/main/java/de/neuefische/backend/model/QuizSession.java
@@ -1,4 +1,4 @@
-package de.neuefische.backend.service;
+package de.neuefische.backend.model;
 
 import de.neuefische.backend.model.Question;
 import de.neuefische.backend.model.QuizParameter;
@@ -7,16 +7,26 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
 public class QuizSession {
     private String sessionId;
     private Question[] questionRepo;
     private Boolean[] scoreRepo;
     QuizParameter quizParameter;
 
+    WebClient webClient = WebClient.create();
+
     public QuizSession(QuizParameter quizParameter){
         this.quizParameter = quizParameter;
 
-        WebClient webClient = WebClient.create();
     }
+
+    /*
+    private Question[] getQuestionsTriviaAPI(QuizParameter quizParameter){
+        //Question[] response =
+    }
+
+     */
+
 
 }
