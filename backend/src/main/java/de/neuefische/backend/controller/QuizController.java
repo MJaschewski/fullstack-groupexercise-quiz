f33233da784d70caf8f563d72d4ce22e6fb06887
bjoern <neuefische@idspispopd.de>
2023-05-17 18:56:38 +0200

    Change Url encoding of postmapping controller for QuizController

--- backend/src/main/java/de/neuefische/backend/controller/QuizController.java
+++ backend/src/main/java/de/neuefische/backend/controller/QuizController.java
@@ -2,6 +2,7 @@ package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.QuizParameter;
 import de.neuefische.backend.service.QuizService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +14,7 @@ public class QuizController {
     public QuizController(QuizService service) {
         this.service = service;
     }
-    @PostMapping("/home")
+    @PostMapping(path="/home",consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     public void PostHome(@RequestBody QuizParameter quizParameter) {
         service.CreateQuizSession(quizParameter.getDifficulty(), quizParameter.getCategory(), quizParameter.getNumQuestions());
     }
