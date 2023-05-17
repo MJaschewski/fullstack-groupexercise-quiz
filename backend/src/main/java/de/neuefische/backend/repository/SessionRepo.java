508610955ae55ae3d27be6d5f82a2cb6ddc275cf
mjaschew <dermijasch@gmx.de>
2023-05-17 16:02:32 +0200

    SessionRepo wiring added

--- backend/src/main/java/de/neuefische/backend/repository/SessionRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/SessionRepo.java
@@ -1,12 +1,14 @@
 package de.neuefische.backend.repository;
 
 import de.neuefische.backend.model.Question;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class SessionRepo {
 
     private Map<String, Question> sessionMap = new HashMap<>();
