9c244112a4810d1830f86096df090afc05ee3e3f
bjoern <neuefische@idspispopd.de>
2023-05-17 16:57:37 +0200

    add string to QuizCategoryEnmum

--- backend/src/main/java/de/neuefische/backend/model/QuizCategory.java
+++ backend/src/main/java/de/neuefische/backend/model/QuizCategory.java
@@ -1,7 +1,13 @@
 package de.neuefische.backend.model;
 
 public enum QuizCategory {
-    ENTERTAINMENT_FILM,
-    SPORTS,
-    SCIENCE_AND_NATURE
+    ENTERTAINMENT_FILM("Entertainment: Film"),
+    ENTERTAINMENT_MUSIC("Entertainment: Music"),
+    SPORTS("Sports"),
+    HISTORY("History"),
+    SCIENCE_AND_NATURE("Science & Nature"),
+    GENERAL_KNOWLEDGE("General Knowledge");
+
+    QuizCategory(String s) {
+    }
 }
