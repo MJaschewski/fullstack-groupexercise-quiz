62011264647e775a08ffcc915c290bdc0935a637
bjoern <neuefische@idspispopd.de>
2023-05-17 16:39:16 +0200

    change parameter to enums in QuizService

--- backend/src/main/java/de/neuefische/backend/service/QuizService.java
+++ backend/src/main/java/de/neuefische/backend/service/QuizService.java
@@ -1,10 +1,12 @@
 package de.neuefische.backend.service;
 
+import de.neuefische.backend.model.QuizCategory;
+import de.neuefische.backend.model.QuizDifficulty;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuizService {
-    public void CreateQuizSession(String difficulty, String genre, String type, int questions)
+    public void CreateQuizSession(QuizDifficulty difficulty, QuizCategory category, String type, int questions)
     {
 
     }
