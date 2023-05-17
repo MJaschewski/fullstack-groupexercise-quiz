5146b1e8e883319ea0e0ebbbe92fd52d2b7e68a5
bjoern <neuefische@idspispopd.de>
2023-05-17 16:03:55 +0200

    add QuizService

---
+++ backend/src/main/java/de/neuefische/backend/service/QuizService.java
@@ -0,0 +1,11 @@
+package de.neuefische.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+    public void CreateQuizSession(String difficulty, String genre, String type, int questions)
+    {
+
+    }
+}
