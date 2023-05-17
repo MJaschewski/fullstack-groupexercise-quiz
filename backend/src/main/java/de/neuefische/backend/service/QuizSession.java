e3acbc6ae26b81c1ab00a9f38bb4ba527a741424
mjaschew <dermijasch@gmx.de>
2023-05-17 15:33:57 +0200

    QuizParameter model & QuizSession skeleton added

--- backend/src/main/java/de/neuefische/backend/service/QuizSession.java
+++ backend/src/main/java/de/neuefische/backend/service/QuizSession.java
@@ -1,6 +1,16 @@
 package de.neuefische.backend.service;
 
+import de.neuefische.backend.model.Question;
+import de.neuefische.backend.model.QuizParameter;
+
 public class QuizSession {
-    String SessionId;
-    Quuestion[]
+    private String sessionId;
+    private Question[] questionRepo;
+    private Boolean[] scoreRepo;
+    QuizParameter quizParameter;
+
+    public QuizSession(QuizParameter quizParameter){
+        this.quizParameter = quizParameter;
+    }
+
 }
