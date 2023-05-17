cacc912ef4161c786cbfae567cd1eb5cd252a9c9
bjoern <neuefische@idspispopd.de>
2023-05-17 17:37:31 +0200

    add QuizService webclient query

--- backend/src/main/java/de/neuefische/backend/service/QuizService.java
+++ backend/src/main/java/de/neuefische/backend/service/QuizService.java
@@ -1,13 +1,26 @@
 package de.neuefische.backend.service;
 
+import de.neuefische.backend.model.OpentdbModel;
 import de.neuefische.backend.model.QuizCategory;
 import de.neuefische.backend.model.QuizDifficulty;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Objects;
 
 @Service
 public class QuizService {
-    public void CreateQuizSession(QuizDifficulty difficulty, QuizCategory category, String type, int questions)
+    public void CreateQuizSession(QuizDifficulty difficulty, QuizCategory category, int questions)
     {
-
+        WebClient client = WebClient.create("https://opentdb.com");
+        Objects.requireNonNull(client.get()
+                        .uri("/api.php?amount=" + questions + "&category=" + category.toString() + "&difficulty=" + difficulty.toString() + "&type=multiple")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        .toEntity(OpentdbModel[].class)
+                        .block())
+                .getBody();
     }
 }
