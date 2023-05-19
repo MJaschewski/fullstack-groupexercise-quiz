39b39d5d049574ce875bb45bcdf11adb955d1db8
mjaschew <dermijasch@gmx.de>
2023-05-19 14:22:45 +0200

    QuizCard test. Bug with css

--- frontend/src/components/QuizCard.tsx
+++ frontend/src/components/QuizCard.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
+import 'QuizCard.css';
+
 type Props = {
-    postAnswer(answer:string):()=>void,
+    postAnswer(answer:string):(answer:string)=>void,
     question:string,
     answers:string[]
 }
@@ -9,10 +11,10 @@ function QuizCard(props:Props) {
     return (
         <div className={"QuizCard"}>
             <h1>{props.question}</h1>
-            <button onClick={props.postAnswer(props.answers[0])}> {props.answers[0]}</button>
-            <button onClick={props.postAnswer(props.answers[1])}> {props.answers[1]}</button>
-            <button onClick={props.postAnswer(props.answers[2])}> {props.answers[2]}</button>
-            <button onClick={props.postAnswer(props.answers[3])}> {props.answers[3]}</button>
+            <button onClick={()=>props.postAnswer(props.answers[0])}> {props.answers[0]}</button>
+            <button onClick={()=>props.postAnswer(props.answers[1])}> {props.answers[1]}</button>
+            <button onClick={()=>props.postAnswer(props.answers[2])}> {props.answers[2]}</button>
+            <button onClick={()=>props.postAnswer(props.answers[3])}> {props.answers[3]}</button>
         </div>
     );
 }
