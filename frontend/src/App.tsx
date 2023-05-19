cd6639896e17264544b50c4c389af12ad67f943c
GitChrisCode <chrha.gw2@gmail.com>
2023-05-19 16:51:59 +0200

    Frontend: started css changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,16 @@ import {CategoryType} from "./components/CategoryType";
 
 function App() {
     const [categories, setCategories] = useState<CategoryType[]>([])
-        //const categories = ['Entertainment: Films','Sports','Science & Nature'];
-        const difficultyLevels = ['Easy','Normal','Hard'];
+    const difficultyLevels = ['Easy', 'Normal', 'Hard'];
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/categories')
-        .then(response => response.data)
-        .then(data => setCategories(data.trivia_categories))
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+            .then(response => response.data)
+            .then(data => {
+                setCategories(data.trivia_categories);
+                console.log(data.trivia_categories);
+            })
+            .catch(error => console.log(error));
     }, []);
 
   return (
@@ -23,7 +24,7 @@ function App() {
     <div className={"App"}>
       <header> <h1>Q U I Z M A S T E R 2000</h1></header>
     </div>
-      <div>
+      <div className={"App"}>
        <h3>Create new Quiz:</h3>
         <h4>Choose options:</h4>
           {<QuizForm difficultyLevels={difficultyLevels} categories={categories.map(currentCategory =>currentCategory.name)} questionCount={10}/>}
