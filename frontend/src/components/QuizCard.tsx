8c7c500c2fd1387402ac25f938ab82fb8cb0a90a
mjaschew <dermijasch@gmx.de>
2023-05-19 14:10:43 +0200

    QuizCard added

---
+++ frontend/src/components/QuizCard.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+type Props = {
+    postAnswer(answer:string):()=>void,
+    question:string,
+    answers:string[]
+}
+function QuizCard(props:Props) {
+
+    return (
+        <div className={"QuizCard"}>
+            <h1>{props.question}</h1>
+            <button onClick={props.postAnswer(props.answers[0])}> {props.answers[0]}</button>
+            <button onClick={props.postAnswer(props.answers[1])}> {props.answers[1]}</button>
+            <button onClick={props.postAnswer(props.answers[2])}> {props.answers[2]}</button>
+            <button onClick={props.postAnswer(props.answers[3])}> {props.answers[3]}</button>
+        </div>
+    );
+}
+
+export default QuizCard;
