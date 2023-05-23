b438a1bd3ca800b64a1341358dd29da54d932aa5
mjaschew <dermijasch@gmx.de>
2023-05-23 17:10:54 +0200

    changed Back QuizForm.tsx

--- frontend/src/components/QuizForm.tsx
+++ frontend/src/components/QuizForm.tsx
@@ -1,10 +1,11 @@
 import React, { ChangeEvent, FormEvent, useState, Key } from 'react';
+import {CategoryType} from "./CategoryType";
 import axios from "axios";
 import './QuizForm.css'
 
 type Props = {
     questionCount: number;
-    categories: React.Key[];
+    categories: CategoryType[];
     difficultyLevels: React.Key[];
 };
 
@@ -25,75 +26,81 @@ const QuizForm = ({ questionCount, categories, difficultyLevels }: Props) => {
 
     };
 
+    const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
+
         axios.post('http://localhost:3000/api/home', formData)
             .then((response) => {
+                // Erfolgreiche Verarbeitung der Serverantwort
                 console.log(response.data);
             })
             .catch((error) => {
+                // Fehler beim Posten der Daten
                 console.error(error);
             });
     };
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="questions">Number of questions:
-                <input
-                    type="number"
-                    name="questions"
-                    value={formData.questions}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                        const { value } = event.target;
-                        if (Number(value) > 0) {
-                            handleQuestionChange(event);
-                        } else {
-                            setFormData((prevData) => ({
-                                ...prevData,
-                                questions: "10",
-                            }));
-                        }
-                    }}
-                />
-            </label>
-            <br/>
-            <br/>
-            <label>
-                <p>Choose Category:</p>
-                {categories.map((level: React.Key) => (
-                    <div key={level}>
-                        <input
-                            type="radio"
-                            id={level.toString()}
-                            name="category"
-                            value={level.toString()}
-                            onChange={handleQuestionChange}
-                            checked={formData.category === level.toString()}
-                        />
-                        <label htmlFor={level.toString()}>{level.toString()}</label>
-                    </div>
-                ))}
-            </label>
-            <label>
-                <p>Choose difficulty:</p>
-                {difficultyLevels.map((level: React.Key) => (
-                    <div key={level}>
-                        <input
-                            type="radio"
-                            id={level.toString()}
-                            name="difficulty"
-                            value={level.toString()}
-                            onChange={handleQuestionChange}
-                            checked={formData.difficulty === level.toString()}
-                        />
-                        <label htmlFor={level.toString()}>{level.toString()}</label>
-                    </div>
-                ))}
-            </label>
-            <br/>
-            <button type="submit">Submit Form</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="questions">Number of questions:
+                    <input
+                        type="number"
+                        name="questions"
+                        value={formData.questions}
+                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                            const { value } = event.target;
+                            if (Number(value) > 0) {
+                                handleQuestionChange(event);
+                            } else {
+                                setFormData((prevData) => ({
+                                    ...prevData,
+                                    questions: "10",
+                                }));
+                            }
+                        }}
+                    />
+                </label>
+                <br/>
+                <br/>
+                <label htmlFor="category">Choose a category: </label>
+
+                <select id="category">
+                    <option value="">Please select</option>{
+                    categories.map(currentCategory => {
+                        return <option value={currentCategory.id}> {currentCategory.name} </option>
+                    })
+                }
+                </select>
+
+
+                <label>
+                    <p>Choose difficulty:</p>
+                    {difficultyLevels.map((level: React.Key) => (
+                        <div key={level}>
+                            <input
+                                type="radio"
+                                id={level.toString()}
+                                name="difficulty"
+                                value={level.toString()}
+                                onChange={handleQuestionChange}
+                                checked={formData.difficulty === level.toString()}
+                            />
+                            <label htmlFor={level.toString()}>{level.toString()}</label>
+                        </div>
+                    ))}
+                </label>
+                <br/>
+                <button type="submit">Submit Form</button>
+            </form>
         </div>
     );
 };
