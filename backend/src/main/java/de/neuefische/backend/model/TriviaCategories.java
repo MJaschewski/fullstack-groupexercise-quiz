ed954eef538637937e842cb6792344134a091a10
mjaschew <dermijasch@gmx.de>
2023-05-23 17:00:14 +0200

    Fixed Frontend Post

--- backend/src/main/java/de/neuefische/backend/model/TriviaCategories.java
+++ backend/src/main/java/de/neuefische/backend/model/TriviaCategories.java
@@ -1,9 +1,11 @@
 package de.neuefische.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class TriviaCategories {
     Integer id;
