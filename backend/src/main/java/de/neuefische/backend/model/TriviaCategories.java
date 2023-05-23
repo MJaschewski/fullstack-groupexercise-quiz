a2308931579a3347f9d415055fae3da7a4fee17a
mjaschew <dermijasch@gmx.de>
2023-05-23 16:03:59 +0200

    Fixed missing constructor

--- backend/src/main/java/de/neuefische/backend/model/TriviaCategories.java
+++ backend/src/main/java/de/neuefische/backend/model/TriviaCategories.java
@@ -1,8 +1,10 @@
 package de.neuefische.backend.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class TriviaCategories {
     Integer id;
     String name;
