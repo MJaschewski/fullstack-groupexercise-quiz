a4a376f12d5ff25eb5e70d8c9a62b4eba25e1f86
mjaschew <dermijasch@gmx.de>
2023-05-24 10:52:28 +0200

    IntegrationTest for getMapping added

---
+++ backend/src/test/java/de/neuefische/backend/controller/QuizController_IntegrationTests.java
@@ -0,0 +1,129 @@
+package de.neuefische.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class QuizController_IntegrationTests {
+    @Autowired
+    MockMvc mockMvc;
+
+     @Test
+    void getMapping_getCategoryList_return_200Ok_return_Categories() throws Exception{
+         //Given
+         String apiResponse = """ 
+                    {
+                    "trivia_categories": [
+                        {
+                            "id": 9,
+                            "name": "General Knowledge"
+                        },
+                        {
+                            "id": 10,
+                            "name": "Entertainment: Books"
+                        },
+                        {
+                            "id": 11,
+                            "name": "Entertainment: Film"
+                        },
+                        {
+                            "id": 12,
+                            "name": "Entertainment: Music"
+                        },
+                        {
+                            "id": 13,
+                            "name": "Entertainment: Musicals & Theatres"
+                        },
+                        {
+                            "id": 14,
+                            "name": "Entertainment: Television"
+                        },
+                        {
+                            "id": 15,
+                            "name": "Entertainment: Video Games"
+                        },
+                        {
+                            "id": 16,
+                            "name": "Entertainment: Board Games"
+                        },
+                        {
+                            "id": 17,
+                            "name": "Science & Nature"
+                        },
+                        {
+                            "id": 18,
+                            "name": "Science: Computers"
+                        },
+                        {
+                            "id": 19,
+                            "name": "Science: Mathematics"
+                        },
+                        {
+                            "id": 20,
+                            "name": "Mythology"
+                        },
+                        {
+                            "id": 21,
+                            "name": "Sports"
+                        },
+                        {
+                            "id": 22,
+                            "name": "Geography"
+                        },
+                        {
+                            "id": 23,
+                            "name": "History"
+                        },
+                        {
+                            "id": 24,
+                            "name": "Politics"
+                        },
+                        {
+                            "id": 25,
+                            "name": "Art"
+                        },
+                        {
+                            "id": 26,
+                            "name": "Celebrities"
+                        },
+                        {
+                            "id": 27,
+                            "name": "Animals"
+                        },
+                        {
+                            "id": 28,
+                            "name": "Vehicles"
+                        },
+                        {
+                            "id": 29,
+                            "name": "Entertainment: Comics"
+                        },
+                        {
+                            "id": 30,
+                            "name": "Science: Gadgets"
+                        },
+                        {
+                            "id": 31,
+                            "name": "Entertainment: Japanese Anime & Manga"
+                        },
+                        {
+                            "id": 32,
+                            "name": "Entertainment: Cartoon & Animations"
+                        }
+                    ]
+                }
+                """;
+         //When & Then
+         mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:3000/api/categories"))
+                 .andExpect(MockMvcResultMatchers.status().isOk())
+                 .andExpect(MockMvcResultMatchers.content().json(apiResponse));
+     }
+}
