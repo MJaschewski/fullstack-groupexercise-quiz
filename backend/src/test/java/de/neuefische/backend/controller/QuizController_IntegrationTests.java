b4e64279463d88d4965a9070cfab038781b24911
chris-diesner <chr.diesner@mailbox.com>
2023-05-25 09:34:13 +0200

    added Test for getCategory from api

--- backend/src/test/java/de/neuefische/backend/controller/QuizController_IntegrationTests.java
+++ backend/src/test/java/de/neuefische/backend/controller/QuizController_IntegrationTests.java
@@ -124,4 +124,111 @@ class QuizController_IntegrationTests {
                  .andExpect(MockMvcResultMatchers.status().isOk())
                  .andExpect(MockMvcResultMatchers.content().json(apiResponse));
      }
+     @Test
+     void getMapping_getCategoryList_return_200Ok_return_CategoriesFromApi() throws Exception{
+         mockMvc.perform(MockMvcRequestBuilders.get("https://opentdb.com/api_category.php"))
+                 .andExpect(MockMvcResultMatchers.status().isOk())
+                 .andExpect(MockMvcResultMatchers.content().json("""
+{
+                    "trivia_categories": [
+                        {
+                            "id": 9,
+                            "name": "General Knowledge"
+                        },
+                        {
+                            "id": 10,
+                            "name": "Entertainment: Books"
+                        },
+                        {
+                            "id": 11,
+                            "name": "Entertainment: Film"
+                        },
+                        {
+                            "id": 12,
+                            "name": "Entertainment: Music"
+                        },
+                        {
+                            "id": 13,
+                            "name": "Entertainment: Musicals & Theatres"
+                        },
+                        {
+                            "id": 14,
+                            "name": "Entertainment: Television"
+                        },
+                        {
+                            "id": 15,
+                            "name": "Entertainment: Video Games"
+                        },
+                        {
+                            "id": 16,
+                            "name": "Entertainment: Board Games"
+                        },
+                        {
+                            "id": 17,
+                            "name": "Science & Nature"
+                        },
+                        {
+                            "id": 18,
+                            "name": "Science: Computers"
+                        },
+                        {
+                            "id": 19,
+                            "name": "Science: Mathematics"
+                        },
+                        {
+                            "id": 20,
+                            "name": "Mythology"
+                        },
+                        {
+                            "id": 21,
+                            "name": "Sports"
+                        },
+                        {
+                            "id": 22,
+                            "name": "Geography"
+                        },
+                        {
+                            "id": 23,
+                            "name": "History"
+                        },
+                        {
+                            "id": 24,
+                            "name": "Politics"
+                        },
+                        {
+                            "id": 25,
+                            "name": "Art"
+                        },
+                        {
+                            "id": 26,
+                            "name": "Celebrities"
+                        },
+                        {
+                            "id": 27,
+                            "name": "Animals"
+                        },
+                        {
+                            "id": 28,
+                            "name": "Vehicles"
+                        },
+                        {
+                            "id": 29,
+                            "name": "Entertainment: Comics"
+                        },
+                        {
+                            "id": 30,
+                            "name": "Science: Gadgets"
+                        },
+                        {
+                            "id": 31,
+                            "name": "Entertainment: Japanese Anime & Manga"
+                        },
+                        {
+                            "id": 32,
+                            "name": "Entertainment: Cartoon & Animations"
+                        }
+                    ]
+                }
+"""));
+     }
 }
