9c9548653a540aca60b71ad68a1a034ffecf89b9
chris-diesner <chr.diesner@mailbox.com>
2023-05-26 13:30:31 +0200

    fixed keys in QuestionCard.tsx

--- frontend/src/components/QuestionCard.tsx
+++ frontend/src/components/QuestionCard.tsx
@@ -4,7 +4,7 @@ import {Question} from "./QuestionType";
 function QuestionCard(props: Question) {
     return (
         <div>
-            <h3>key={props.description}</h3>
+            <h3>{props.description}</h3>
             {props.answers.map(currentAnswer => {
                 return <p key={'index'}>{currentAnswer}</p>
             })}
