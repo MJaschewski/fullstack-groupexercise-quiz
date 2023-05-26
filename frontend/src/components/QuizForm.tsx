ba4f6ab18ac40332737e782a18e16c4516fe75a6
chris-diesner <chr.diesner@mailbox.com>
2023-05-26 10:23:02 +0200

    include Routing for current features so far

--- frontend/src/components/QuizForm.tsx
+++ frontend/src/components/QuizForm.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import axios from "axios";
 import './QuizForm.css'
 
@@ -8,7 +8,7 @@ type Props = {
     difficultyLevels: React.Key[];
 };
 
-const QuizForm = ({ questionCount, categories, difficultyLevels }: Props) => {
+const QuizForm = ({questionCount, categories, difficultyLevels}: Props) => {
     const [formData, setFormData] = useState({
         questions: '',
         category: '',
@@ -38,62 +38,66 @@ const QuizForm = ({ questionCount, categories, difficultyLevels }: Props) => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="questions">Number of questions:
-                    <input
-                        type="number"
-                        name="questions"
-                        value={formData.questions}
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                            const { value } = event.target;
-                            if (Number(value) > 0) {
-                                handleQuestionChange(event);
-                            } else {
-                                setFormData((prevData) => ({
-                                    ...prevData,
-                                    questions: "10",
-                                }));
-                            }
-                        }}
-                    />
-                </label>
-                <br/>
-                <br/>
-                <label>
-                    <p>Choose Category:</p>
-                    {categories.map((level: React.Key) => (
-                        <div key={level}>
-                            <input
-                                type="radio"
-                                id={level.toString()}
-                                name="category"
-                                value={level.toString()}
-                                onChange={handleQuestionChange}
-                                checked={formData.category === level.toString()}
-                            />
-                            <label htmlFor={level.toString()}>{level.toString()}</label>
-                        </div>
-                    ))}
-                </label>
-                <label>
-                    <p>Choose difficulty:</p>
-                    {difficultyLevels.map((level: React.Key) => (
-                        <div key={level}>
-                            <input
-                                type="radio"
-                                id={level.toString()}
-                                name="difficulty"
-                                value={level.toString()}
-                                onChange={handleQuestionChange}
-                                checked={formData.difficulty === level.toString()}
-                            />
-                            <label htmlFor={level.toString()}>{level.toString()}</label>
-                        </div>
-                    ))}
-                </label>
-                <br/>
-                <button type="submit">Submit Form</button>
-            </form>
+            <div className={"QuizSelection"}>
+                <h3>Create new Quiz:</h3>
+                <h4>Choose options:</h4>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="questions">Number of questions:
+                        <input
+                            type="number"
+                            name="questions"
+                            value={formData.questions}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                const {value} = event.target;
+                                if (Number(value) > 0) {
+                                    handleQuestionChange(event);
+                                } else {
+                                    setFormData((prevData) => ({
+                                        ...prevData,
+                                        questions: "10",
+                                    }));
+                                }
+                            }}
+                        />
+                    </label>
+                    <br/>
+                    <br/>
+                    <label>
+                        <p>Choose Category:</p>
+                        {categories.map((level: React.Key) => (
+                            <div key={level}>
+                                <input
+                                    type="radio"
+                                    id={level.toString()}
+                                    name="category"
+                                    value={level.toString()}
+                                    onChange={handleQuestionChange}
+                                    checked={formData.category === level.toString()}
+                                />
+                                <label htmlFor={level.toString()}>{level.toString()}</label>
+                            </div>
+                        ))}
+                    </label>
+                    <label>
+                        <p>Choose difficulty:</p>
+                        {difficultyLevels.map((level: React.Key) => (
+                            <div key={level}>
+                                <input
+                                    type="radio"
+                                    id={level.toString()}
+                                    name="difficulty"
+                                    value={level.toString()}
+                                    onChange={handleQuestionChange}
+                                    checked={formData.difficulty === level.toString()}
+                                />
+                                <label htmlFor={level.toString()}>{level.toString()}</label>
+                            </div>
+                        ))}
+                    </label>
+                    <br/>
+                    <button type="submit">Submit Form</button>
+                </form>
+            </div>
         </div>
     );
 };
