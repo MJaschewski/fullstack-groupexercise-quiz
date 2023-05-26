123c74471f1c81758c63b11e2c3d285a807887a2
chris-diesner <chr.diesner@mailbox.com>
2023-05-26 16:29:25 +0200

    merged with single question display

--- frontend/src/components/Questions.tsx
+++ frontend/src/components/Questions.tsx
@@ -3,11 +3,28 @@ import React, {useEffect, useState} from 'react';
 import {Question} from "./QuestionType";
 import QuestionCard from "./QuestionCard";
 import axios from "axios";
+import {UserAnswer} from "./UserAnswerType";
 
 
 const Questions = () => {
 
     const [questionsUnsortedList, setQuestionsUnsortedList] = useState<Question[]>([])
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const [userAnswers, setUsersAnswer] = useState<UserAnswer[]>([])
+    const handleNext =() => {
+        setCurrentIndex((prevIndex) => prevIndex + 1)
+    }
+
+    function setSingleAnswer(submittedAnswer:UserAnswer){
+        setUsersAnswer(userAnswers.map(currentAnswer => {
+            if(currentAnswer.description === submittedAnswer.description ){
+                return submittedAnswer;
+            }else  {
+                return currentAnswer;
+            }
+        }))
+    }
+
     useEffect(() => {
         axios.get('/api/questions')
             .then(response => response.data)
@@ -15,16 +32,27 @@ const Questions = () => {
                 setQuestionsUnsortedList(data);
                 console.log(data);
             })
+            .then(
+                () => setUsersAnswer(questionsUnsortedList.map(currentQuestion => {
+                    return  {description:currentQuestion.description,answer:""};
+                }))
+            )
             .catch(error => console.log(error));
     }, []);
+
+    const currentQuestion = questionsUnsortedList[currentIndex];
 
 
     return (
         <div>
-            {questionsUnsortedList.map((currentQuestion: Question) => <QuestionCard
-                key={"questionCard_" + currentQuestion.description}
-                description={currentQuestion.description}
-                answers={currentQuestion.answers}/>)}
+            {currentQuestion && (
+                <QuestionCard
+                    key={"questionCard_" + currentQuestion.description}
+                    setSingleAnswer={setSingleAnswer}
+                    description={currentQuestion.description}
+                    answers={currentQuestion.answers}/>
+            )}
+            <button onClick={handleNext}>Next</button>
         </div>
     );
 }
