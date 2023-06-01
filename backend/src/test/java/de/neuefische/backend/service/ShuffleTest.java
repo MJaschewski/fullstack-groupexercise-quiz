a32d60b3b22cd679fc1e2c87b6f0905eb8fb98ed
chris-diesner <chr.diesner@mailbox.com>
2023-06-01 17:54:00 +0200

    fixed shuffle test

--- backend/src/test/java/de/neuefische/backend/service/ShuffleTest.java
+++ backend/src/test/java/de/neuefische/backend/service/ShuffleTest.java
@@ -12,14 +12,7 @@ public class ShuffleTest {
     public void testShuffle() {
         List<String> unsortedAnswers = Arrays.asList("Answer 1", "Answer 2", "Answer 3", "Answer 4");
         List<String> originalOrder = new ArrayList<>(unsortedAnswers);
-
-        // Shuffle the answers
         Collections.shuffle(unsortedAnswers);
-
-        // Assert that the order has changed
-        Assert.assertNotEquals(originalOrder, unsortedAnswers);
-
-        // Assert that the shuffled list contains the same elements
         Assert.assertTrue(unsortedAnswers.containsAll(originalOrder) && originalOrder.containsAll(unsortedAnswers));
     }
 }
