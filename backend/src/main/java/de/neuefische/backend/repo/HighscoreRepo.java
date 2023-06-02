f3b0e4c7e2b423d184422f3770ddf1958b1e7377
mjaschew <dermijasch@gmx.de>
2023-06-02 11:29:16 +0200

    Pom.xml right mongo dependency

--- backend/src/main/java/de/neuefische/backend/repo/HighscoreRepo.java
+++ backend/src/main/java/de/neuefische/backend/repo/HighscoreRepo.java
@@ -2,10 +2,6 @@ package de.neuefische.backend.repo;
 
 import de.neuefische.backend.model.Result;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 
 public interface HighscoreRepo extends MongoRepository<Result, String> {
 }
