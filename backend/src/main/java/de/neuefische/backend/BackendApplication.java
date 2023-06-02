8978d1bbc6d33142a032faf296eb6e71764d09a7
mjaschew <dermijasch@gmx.de>
2023-06-02 11:24:36 +0200

    Hotfix no constructors in HighScoreService

--- backend/src/main/java/de/neuefische/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package de.neuefische.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("de.neuefische.backend.repo")
 public class BackendApplication {
 
     public static void main(String[] args) {
