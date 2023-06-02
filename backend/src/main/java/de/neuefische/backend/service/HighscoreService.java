aeb2cb2def188425c9fdf38daf99d6ce6d081ad7
mjaschew <dermijasch@gmx.de>
2023-06-02 10:29:31 +0200

    BackendLogic for HighscoreBoard

--- backend/src/main/java/de/neuefische/backend/service/HighscoreService.java
+++ backend/src/main/java/de/neuefische/backend/service/HighscoreService.java
@@ -1,6 +1,7 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.model.Result;
+import de.neuefische.backend.model.ResultDTO;
 import de.neuefische.backend.repo.HighscoreRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,15 @@ public class HighscoreService {
 
     private final HighscoreRepo highscoreRepo;
 
+    private final UUIDService uuidService;
 
     public List<Result> getResults() {
-        return null;
+        return highscoreRepo.findAll();
     }
 
-    public String addResult(Result highscore) {
-        return null;
+    public String addResult(ResultDTO resultDTO) {
+        Result highscore = new Result(uuidService.generateUUID(), resultDTO.getPlayerName(), resultDTO.getScore(), resultDTO.getDifficulty(), resultDTO.getCategory(), resultDTO.getNumOfQuestions());
+        highscoreRepo.save(highscore);
+        return Boolean.toString(highscoreRepo.existsById(highscore.getId()));
     }
 }
