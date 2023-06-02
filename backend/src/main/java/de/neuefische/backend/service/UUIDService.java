4fd5bd219572bbd0c635c7dd7ecb131b22df0218
mjaschew <dermijasch@gmx.de>
2023-06-02 10:49:35 +0200

    Hotfix no constructors in HighScoreService

--- backend/src/main/java/de/neuefische/backend/service/UUIDService.java
+++ backend/src/main/java/de/neuefische/backend/service/UUIDService.java
@@ -1,14 +1,11 @@
 package de.neuefische.backend.service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
 public class UUIDService {
     public String generateUUID() {
         return UUID.randomUUID().toString();
